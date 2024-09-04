bebac091ccc4c22234744f45ee38271d768cabf2
Mucino11 <64760426+Mucino11@users.noreply.github.com>
2024-09-04 10:00:30 +0200

    refactoring

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -1,6 +1,2 @@
-// ./src/config/index.ts
-
-// export { port };
-
 // const port = 3000;
 export const port = 3000;
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,4 @@
 // // ./src/index.ts
-
-// import app from "./app";
-// import { port } from "./config";
-
-// import { serve } from "@hono/node-server";
-
-// console.log(`Server is running on port ${port}`);
-
-// serve({
-//   fetch: app.fetch,
-//   port,
-// });
 import app from "./app";
 import { port } from "./config";
 
